f83ef12c6e796b84c17c45492a98a452ef8a5db3
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-12-19 14:34:49 +0700

    refactor: improve structure and naming in index.js
    - Renamed form element IDs for clarity (name, age, money)
    - Adjusted table layout for better organization
    - Restructured code for improved readability and maintainability

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Registrasi & List Pendaftar</title>
+    <title>registration & List data_register</title>
     <!-- Bootstrap CSS for styling -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
@@ -15,38 +15,38 @@
         <!-- Tab Navigation -->
         <ul class="nav nav-tabs" id="myTabs" role="tablist">
             <li class="nav-item" role="presentation">
-                <a class="nav-link active" id="registrasi-tab" data-bs-toggle="tab" href="#registrasi" role="tab"
-                    aria-controls="registrasi" aria-selected="true">Registrasi</a>
+                <a class="nav-link active" id="registration-tab" data-bs-toggle="tab" href="#registration" role="tab"
+                    aria-controls="registration" aria-selected="true">Registrasi</a>
             </li>
             <li class="nav-item" role="presentation">
-                <a class="nav-link" id="list-pendaftar-tab" data-bs-toggle="tab" href="#list-pendaftar" role="tab"
-                    aria-controls="list-pendaftar" aria-selected="false">List Pendaftar</a>
+                <a class="nav-link" id="list-data_register-tab" data-bs-toggle="tab" href="#list-data_register" role="tab"
+                    aria-controls="list-data_register" aria-selected="false">List Data</a>
             </li>
         </ul>
 
         <!-- Tab Content -->
         <div class="tab-content" id="myTabsContent">
-            <!-- Tab Registrasi -->
-            <div class="tab-pane fade show active" id="registrasi" role="tabpanel" aria-labelledby="registrasi-tab">
+            <!-- Tab Registration -->
+            <div class="tab-pane fade show active" id="registration" role="tabpanel" aria-labelledby="registration-tab">
                 <form id="registration-form">
                     <div class="mb-3">
-                        <label for="nama" class="form-label">Nama</label>
-                        <input type="text" class="form-control" id="nama" required>
+                        <label for="name" class="form-label">Nama</label>
+                        <input type="text" class="form-control" id="name" required>
                     </div>
                     <div class="mb-3">
-                        <label for="umur" class="form-label">Umur</label>
-                        <input type="number" class="form-control" id="umur" required>
+                        <label for="age" class="form-label">Umur</label>
+                        <input type="number" class="form-control" id="age" required>
                     </div>
                     <div class="mb-3">
-                        <label for="uang-sangu" class="form-label">Uang Sangu</label>
-                        <input type="number" class="form-control" id="uang-sangu" required>
+                        <label for="money" class="form-label">Uang Sangu</label>
+                        <input type="number" class="form-control" id="money" required>
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
 
-            <!-- Tab List Pendaftar -->
-            <div class="tab-pane fade" id="list-pendaftar" role="tabpanel" aria-labelledby="list-pendaftar-tab">
+            <!-- Tab List data_register -->
+            <div class="tab-pane fade" id="list-data_register" role="tabpanel" aria-labelledby="list-data_register-tab">
                 <div id="resume"></div>
                 <table class="table table-bordered mt-3">
                     <thead>
@@ -56,7 +56,7 @@
                             <th class="col-4">Uang Sangu</th>
                         </tr>
                     </thead>
-                    <tbody id="pendaftar-list"></tbody>
+                    <tbody id="data_register-list"></tbody>
                 </table>
             </div>
         </div>
@@ -67,7 +67,7 @@
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
     </script>
 
-    <!-- Sertakan JavaScript terpisah -->
+    <!-- Vanilla Javascript -->
     <script src="script.js"></script>
 </body>
 
