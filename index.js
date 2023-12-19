621c234c4b36a2fb39a7fcb38d88d326ab21ba91
Wawah Sri Sunenti <wawahsrisunenti@gmail.com>
2023-12-19 15:30:51 +0700

    style: update tab names, headings, and language consistency in index.js
    - Renamed tab and heading for better clarity (list-pendaftar to List Registrants)
    - Updated label for 'Uang Sangu' to include currency (Uang Sangu (Rp))
    - Ensured language consistency in headings and labels
    - Improved overall code readability and maintainability

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>registration & List data_register</title>
-    <!-- Bootstrap CSS for styling -->
+    <title>Registration & List of Registrants</title>
+    <!-- Bootstrap CSS untuk styling -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
 </head>
@@ -16,17 +16,17 @@
         <ul class="nav nav-tabs" id="myTabs" role="tablist">
             <li class="nav-item" role="presentation">
                 <a class="nav-link active" id="registration-tab" data-bs-toggle="tab" href="#registration" role="tab"
-                    aria-controls="registration" aria-selected="true">Registrasi</a>
+                    aria-controls="registration" aria-selected="true">Registration</a>
             </li>
             <li class="nav-item" role="presentation">
-            <a class="nav-link" id="list-data_register-tab" data-bs-toggle="tab" href="#list-data_register"
-                    role="tab" aria-controls="list-data_register" aria-selected="false">List Data</a>
+                <a class="nav-link" id="list-pendaftar-tab" data-bs-toggle="tab" href="#list-pendaftar" role="tab"
+                    aria-controls="list-pendaftar" aria-selected="false">List Registrants</a>
             </li>
         </ul>
 
         <!-- Tab Content -->
         <div class="tab-content" id="myTabsContent">
-            <!-- Tab Registration -->
+            <!-- Tab Registrasi -->
             <div class="tab-pane fade show active" id="registration" role="tabpanel" aria-labelledby="registration-tab">
                 <form id="registration-form">
                     <div class="mb-3">
@@ -38,37 +38,38 @@
                         <input type="number" class="form-control" id="age" required>
                     </div>
                     <div class="mb-3">
-                        <label for="money" class="form-label">Uang Sangu</label>
+                        <label for="money" class="form-label">Uang Sangu (Rp)</label>
                         <input type="number" class="form-control" id="money" required>
                     </div>
                     <button type="submit" class="btn btn-primary">Submit</button>
                 </form>
             </div>
 
-            <!-- Tab List data_register -->
-            <div class="tab-pane fade" id="list-data_register" role="tabpanel" aria-labelledby="list-data_register-tab">
-                <div id="resume"></div>
+            <!-- Tab List Pendaftar -->
+            <div class="tab-pane fade" id="list-pendaftar" role="tabpanel" aria-labelledby="list-pendaftar-tab">
                 <table class="table table-bordered mt-3">
                     <thead>
                         <tr>
-                            <th class="col-5" scope="col">Nama</th>
-                            <th class="col-3" scope="col">Umur</th>
-                            <th class="col-4" scope="col">Uang Sangu</th>
+                            <th class="col-5">Nama</th>
+                            <th class="col-3">Umur</th>
+                            <th class="col-4">Uang Sangu</th>
                         </tr>
                     </thead>
-                    <tbody id="data_register-list"></tbody>
+                    <tbody id="pendaftar-list"></tbody>
                 </table>
+                <h3 id="average-age"></h3>
+                <h3 id="average-money"></h3>
             </div>
         </div>
     </div>
 
-    <!-- Js of Bootstrap to give animation style -->
+    <!-- Bootstrap JS untuk efek tab -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous">
     </script>
 
-    <!-- Vanilla Javascript -->
-    <script src="js/records.js"></script>
+    <!-- JavaScript kustom -->
+    <script src="JavaScript/data.js"></script>
 </body>
 
 </html>
